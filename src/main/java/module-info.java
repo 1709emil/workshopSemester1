547540b43b2ac06ae46254e245f974199f257aeb
Emil Kræmmer Jørgensen <1709emil@gmail.com>
2022-11-23 13:29:55 +0100

    intit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.workshopsemester1 {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.workshopsemester1 to javafx.fxml;
     exports com.example.workshopsemester1;
 }
